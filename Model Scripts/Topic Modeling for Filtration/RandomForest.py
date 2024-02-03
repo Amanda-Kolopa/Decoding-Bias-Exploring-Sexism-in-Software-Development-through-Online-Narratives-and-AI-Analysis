8586097a39595670aaaf5d0a3ba30dbaa09e7771
Amanda-Kolopa <19229712+Amanda-Kolopa@users.noreply.github.com>
2024-02-02 20:50:14 -0500

    fix: removed hot encoding for probability values

--- Model Scripts/Topic Modeling for Filtration/RandomForest.py
+++ Model Scripts/Topic Modeling for Filtration/RandomForest.py
@@ -52,43 +52,19 @@ X_train = experiment_data[['Top Topic using Post Title - Word 1'] +
                           ['Probability 3 using Dataset']
 ]
 
+# One-hot encoding for String column data
 X_train = pd.get_dummies(X_train, columns=['Top Topic using Post Title - Word 1',
                                            'Top Topic using Post Title - Word 2',
                                            'Top Topic using Post Title - Word 3',
                                            'Top Topic using Post Title - Word 4',
                                            'Top Topic using Post Title - Word 5',
-                                           'Probability using Post Title',
                                            'Top Topic 1 using Vocabulary',
-                                           'Probability 1 using Vocabulary',
                                            'Top Topic 2 using Vocabulary',
-                                           'Probability 2 using Vocabulary',
                                            'Top Topic 3 using Vocabulary',
-                                           'Probability 3 using Vocabulary',
                                            'Top Topic 1 using Dataset',
-                                           'Probability 1 using Dataset',
                                            'Top Topic 2 using Dataset',
-                                           'Probability 2 using Dataset',
-                                           'Top Topic 3 using Dataset',
-                                           'Probability 3 using Dataset'])
-# X_train['Top Topic using Post Title - Word 2'] = pd.get_dummies(X_train, columns=['Top Topic using Post Title - Word 2'])
-# X_train['Top Topic using Post Title - Word 3'] = pd.get_dummies(X_train, columns=['Top Topic using Post Title - Word 3'])
-# X_train['Top Topic using Post Title - Word 4'] = pd.get_dummies(X_train, columns=['Top Topic using Post Title - Word 4'])
-# X_train['Top Topic using Post Title - Word 5'] = pd.get_dummies(X_train, columns=['Top Topic using Post Title - Word 5'])
-# X_train['Probability using Post Title'] = pd.get_dummies(X_train, columns=['Probability using Post Title'])
-#
-# X_train['Top Topic 1 using Vocabulary'] = pd.get_dummies(X_train, columns=['Top Topic 1 using Vocabulary'])
-# X_train['Probability 1 using Vocabulary'] = pd.get_dummies(X_train, columns=['Probability 1 using Vocabulary'])
-# X_train['Top Topic 2 using Vocabulary'] = pd.get_dummies(X_train, columns=['Top Topic 2 using Vocabulary'])
-# X_train['Probability 2 using Vocabulary'] = pd.get_dummies(X_train, columns=['Probability 2 using Vocabulary'])
-# X_train['Top Topic 3 using Vocabulary'] = pd.get_dummies(X_train, columns=['Top Topic 3 using Vocabulary'])
-# X_train['Probability 3 using Vocabulary'] = pd.get_dummies(X_train, columns=['Probability 3 using Vocabulary'])
-#
-# X_train['Top Topic 1 using Dataset'] = pd.get_dummies(X_train, columns=['Top Topic 1 using Dataset'])
-# X_train['Probability 1 using Dataset'] = pd.get_dummies(X_train, columns=['Probability 1 using Dataset'])
-# X_train['Top Topic 2 using Dataset'] = pd.get_dummies(X_train, columns=['Top Topic 2 using Dataset'])
-# X_train['Probability 2 using Dataset'] = pd.get_dummies(X_train, columns=['Probability 2 using Dataset'])
-# X_train['Top Topic 3 using Dataset'] = pd.get_dummies(X_train, columns=['Top Topic 3 using Dataset'])
-# X_train['Probability 3 using Dataset'] = pd.get_dummies(X_train, columns=['Probability 3 using Dataset'])
+                                           'Top Topic 3 using Dataset'
+                                           ])
 print(X_train)
 
 # X_train = experiment_data.iloc[:, :-1]
@@ -98,11 +74,18 @@ y_train = experiment_data['Target']
 X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
 
 # Train RandomForest model
-random_forest = RandomForestClassifier(n_estimators=100, max_depth=3, random_state=42)
+random_forest = RandomForestClassifier(n_estimators=100, random_state=42)
 random_forest.fit(X_train_split, y_train_split)
 
-# Find the tree with the highest information gain (lowest impurity)
-best_tree_index = random_forest.feature_importances_.argmax()
+# Find the tree with the highest accuracy on the training set
+best_tree_index = None
+best_tree_accuracy = 0.0
+
+for i, tree in enumerate(random_forest.estimators_):
+    accuracy = tree.score(X_train, y_train)
+    if accuracy > best_tree_accuracy:
+        best_tree_accuracy = accuracy
+        best_tree_index = i
 
 # Output the rules of the best decision tree
 best_tree = random_forest.estimators_[best_tree_index]
@@ -112,6 +95,7 @@ print(f"Best Decision Tree (index {best_tree_index}):\n{tree_rules}")
 # Visualize the best decision tree
 plt.figure(figsize=(12, 8))
 plot_tree(best_tree, feature_names=list(X_train.columns), class_names=True, filled=True, rounded=True)
+plt.savefig('C:/Users/amand/OneDrive/Desktop/Thesis/Updated_Thesis/Model Scripts/Topic Modeling for Filtration/Best Tree - TI.pdf')
 plt.show()
 
 # Make predictions on the testing data
